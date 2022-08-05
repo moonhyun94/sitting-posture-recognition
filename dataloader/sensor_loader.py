7a475375e6997d1dc73890d56252af3df7c74f68
enter1994@naver.com <enter1994@naver.com>
2022-08-05 08:08:25 +0000

    Sensor_Model

---
+++ dataloader/sensor_loader.py
@@ -0,0 +1,70 @@
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+import numpy as np
+from PIL import Image
+import pathlib
+import os
+import glob
+
+# data.shape = (N, 3, 5, 224, 224)
+
+# hyperparameter
+INPUT_SIZE = 512
+FRAME = 5
+
+# DataLoader
+class CustomDataset(Dataset):
+    def __init__(self, mode='Train'):
+        super(CustomDataset, self).__init__()
+        
+        """
+        :param folders: list of all the video folders
+        :param frames: start frame, end frame and skip frame numpy array
+        :param to_augment: boolean if the data is suppose to augment
+        :param transform: transform function
+        :param mode: train/test
+        """
+
+        self.data = []
+        self.label = []
+        self.mode = mode
+
+        self.image_rgb = transforms.Compose([
+            transforms.Resize([INPUT_SIZE, INPUT_SIZE]),
+            transforms.ToTensor()
+        ])
+        #self.transform = transforms.ToTensor()
+
+        for name in pathlib.Path(f'./data/{mode}/').glob('*.jpg'):
+            category = os.path.splitext(name)[0].split('/')[2]
+            self.label.append(category)
+
+            self.data.append((str(name), int(category[1])))
+            self.data = sorted(self.data)
+
+
+
+    def __len__(self):
+        return len(self.data) // FRAME
+
+    def __getitem__(self, idx):
+        image_list = []
+        
+        for num in range(idx*FRAME, (idx+1)*FRAME):
+            path = self.data[num][0]
+            image = Image.open(f'./{path}')
+
+            # (3,224,224)
+            image = self.image_rgb(image)
+            image_list.append(image)            
+        
+        # (3,224,224) -> (3,5,224,224)
+        image_list = torch.stack(image_list, dim=1)
+
+        label = torch.tensor(self.data[idx*FRAME][1])
+
+        return image_list, label
+
+    
---
+++ sensor_train.py
@@ -0,0 +1,92 @@
+from pickletools import optimize
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from dataloader import CustomDataset
+from model import C3D
+
+from tqdm import tqdm
+import os
+import pathlib
+
+BATCH_SIZE = 1
+NUM_CLASSES = 6
+LEARNING_RATE = 1e-4
+EPOCHS = 20
+
+
+def train(dataloader, model, loss, optimizer):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+
+    model.train()
+    
+    train_loss, correct = 0, 0
+
+    for batch, (x, y) in enumerate(dataloader):
+        
+
+        optimizer.zero_grad()
+        x, y = x.to(device), y.to(device)
+
+        pred = model(x)
+        loss = loss_fn(pred, y)
+
+        loss.backward()
+        optimizer.step()
+
+        loss = loss.item()
+        train_loss += loss
+
+        correct += (pred.argmax(1)==y).type(torch.float).mean().item()
+
+    train_loss /= num_batches
+    correct /= size
+
+    print(f'loss: {train_loss:>7f}\n Train Accuracy : {(100*correct):>8f}')
+
+def test(dataloader, model, loss):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+
+    model.eval()
+
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+
+            pred = model(x)
+            test_loss += loss_fn(pred, y).item()
+
+            correct += (pred.argmax(1)==y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+
+    print(f'Test Accuracy : {(100*correct):>0.1f}%, Avg loss : {test_loss:>8f} \n')
+
+if __name__=="__main__":
+
+
+    device = torch.device('cuda:1')
+    my_model = C3D(NUM_CLASSES, pretrained=True).to(device)
+
+    train_dataset = CustomDataset(mode='Train')
+    test_dataset = CustomDataset(mode='Test')
+
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(my_model.parameters(), lr=LEARNING_RATE)
+
+    for epoch in tqdm(range(EPOCHS)):
+        print(f'Epoch : {epoch+1} \n--------------------------------')
+
+        train(train_dataloader, my_model, loss_fn, optimizer)
+        test(test_dataloader, my_model, loss_fn)
+
+    print('Done')
